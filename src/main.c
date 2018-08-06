34d410b64f7ead62896089057dd211ad76512245
j <info@kondozer.org>
2018-08-06 18:10:47 +0200

    commandline arguments

--- src/main.c
+++ src/main.c
@@ -11,80 +11,21 @@
 
 int main(int argc, char* argv[])
 {
-	Raw_wave * wave = merge_waves_autovalues("../../audio");	
-	write_wave(wave, "../../output/output.wav");
-	destroy_wave(&wave);
-
 	/**
-	Raw_wave * wave;
-	load_wave(&wave, "../../audio/1.wav");
-	print_wave(wave, false);
-	destroy_wave(&wave);
+	commandline arguments: 
+	1: input folder
+	2: output file path
+	3: minimal amount or random samples from input file
+	4: maxium amount or random samples from input file
+	5: minimal offset from input file
+	6: maxium offset from input file
 	**/
 
-	/**
-	Raw_wave * container = create_header();
-	Raw_wave * wave;
-	load_wave(&wave, "../../audio/1.wav");
-	int srcAmount = 60000;
-	//int srcOffset = random(10000, 60000);
-	insert_samples(container, wave, srcAmount, 0, 0, false);
-	print_wave(container);
-
+	printf("Merging waves..\n");
+	Raw_wave * wave = merge_waves(argv[1], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
+	write_wave(wave, argv[2]);
 	destroy_wave(&wave);
-	destroy_wave(&container);
-	printf("------------------------------------------------------------------------\n");
-	Raw_wave * containerTwo = create_header();
-	load_wave(&wave, "../../audio/1.wav");
-	srcAmount = 240000;
-	//int srcOffset = random(10000, 60000);
-	int srcOffset = num_samples(wave) /2;
-	insert_samples(containerTwo, wave, srcAmount, srcOffset, 0, false);
-
-	write_wave(containerTwo, "../../audio/output.wav");
-
-
-	print_wave(containerTwo);
-	**/
-
-	/**
-	Linked_list * list = llist_create();
-	llist_push(&list, 0);
-	llist_push(&list, 1);
-	llist_push(&list, 2);
-	llist_print(&list);
-
-	printf("loading .wavs..\n");
-	Raw_wave * waveOne;
-	if (!load_wave(&waveOne, "../../audio/4.wav")) {
-		printf("Could not init wave one");
-	}
-
-	Raw_wave * waveTwo;
-	if (!load_wave(&waveTwo, "../../audio/5.wav")) {
-		printf("Could not init wave two");
-	}
-
-	printf("===================\n");
-	print_wave(waveTwo);
-	printf("===================\n");
-
-	printf("merging .wavs..\n");
-	//insert_samples(waveOne, waveTwo, num_samples(waveTwo), num_samples(waveTwo)/2, num_samples(waveOne) /2, false);
-	int srcAmount = 60000;
-	//int srcOffset = random(10000, 60000);
-	int srcOffset = num_samples(waveOne) / 2;
-	insert_samples(waveOne, waveTwo, srcAmount, srcOffset, num_samples(waveOne), false);
-
-	printf("===================\n");
-	print_wave(waveOne);
-	printf("===================\n");
-
-	printf("Writing .wav to disk..\n");
-	write_wave(waveOne, "../../audio/output.wav");
-
 	printf("Done\n");
-	**/
 	getchar();	
 
 	return 0;
