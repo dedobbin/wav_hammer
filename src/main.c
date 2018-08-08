0dfd30601498c4c30410d7a1a3b9eeaf74e8ba50
j <info@kondozer.org>
2018-08-08 19:44:11 +0200

    wip

--- src/main.c
+++ src/main.c
@@ -19,6 +19,10 @@ int main(int argc, char* argv[])
 	4: maxium amount or random samples from input file
 	5: minimal offset from input file
 	6: maxium offset from input file
+
+	OR 
+
+	1: config file
 	**/
 
 	int result = 0;
@@ -36,22 +40,34 @@ int main(int argc, char* argv[])
 	destroy_wave(&wave);
 	**/
 	
-	if (argc != 7) {
-		printf("No arguments given..\n");
-		printf("Commandline arguments:\n");
-		printf("1:\tinput folder\n2: \toutput file path\n3: \tminimal amount or random samples from input file\n4: \tmaxium amount or random samples from input file\n5: \tminimal offset from input file\n6: \tmaxium offset from input file");
-		result = 1;
-	} else {
+	if (argc == 7) {
 		printf("Merging waves..\n");
 		Raw_wave * wave = merge_waves(argv[1], atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
 		printf("Saving wave file to %s..\n", argv[2]);
 		hamming_pointless_gain(wave);
 		write_wave(wave, argv[2]);
 		destroy_wave(&wave);
-		printf("Edn\n");
 		result = 0;
+	} else if (argc == 2) {
+		FILE *fp;
+		char * c = malloc(1000);
+		int i = 0;
+		if (fp = fopen(argv[1], "r") != NULL) {
+			do {
+				*(c + i) = fgetc(fp);
+			} while (*c != EOF);
+		}
+	} else {
+		printf("No arguments given..\n");
+		printf("Commandline arguments:\n");
+		printf("1:\tinput folder\n2: \toutput file path\n3: \tminimal amount or random samples from input file\n4: \tmaxium amount or random samples from input file\n5: \tminimal offset from input file\n6: \tmaxium offset from input file");
+		printf("\nOR\n\n");
+		printf("1: path to config file\n");
+		result = 1;
 	}
+
 	getchar();
+	printf("Edn\n");
 	return result;
 
 }
