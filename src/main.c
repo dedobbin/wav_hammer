826fcd22f6bc74e0029ee5f7a74c41d4a327d0b3
j <info@kondozer.org>
2018-08-26 19:33:16 +0200

    testing

--- src/main.c
+++ src/main.c
@@ -1,5 +1,5 @@
 #include "process_input.h"
-
+#include "raw_wave.h"
 int main(int argc, char* argv[])
 {
 	/**
@@ -15,6 +15,18 @@ int main(int argc, char* argv[])
 
 	1: config file
 	**/
+
+	//void insert_samples(Raw_wave * dst, Raw_wave * src, long src_amount, long src_offset, long dst_offset, bool overwrite);
+	Raw_wave * total = NULL;
+	load_wave(&total, "../../audio/1.wav");
+	
+	Raw_wave * segment = create_header();
+	insert_samples(segment, total, num_samples(total), 0, 0, false);
+	write_wave(segment, "../../output/testing.wav");
+	printf("num samples input : %d\n", num_samples(total));
+	printf("num samples output : %d\n", num_samples(segment));
+	getchar();
+	return 0;
 
 	if (argc > 1) {
 		process_commandline_arguments(argc, argv);
