1403a43f063cfd442c48b2b742ba966f0cc0100a
jody <>
2018-09-09 21:43:47 +0200

    Got rid of insert_samples_VAR warning

--- src/wave_samples.h
+++ src/wave_samples.h
@@ -14,8 +14,11 @@ typedef struct {
 } insert_samples_args;
 
 
+//Included so inser_samples marco does not give warning, could also perhaps use directly
+void insert_samples_VAR(insert_samples_args);
 //When user 'calls' insert_samples(), params are used to populate struct, which is passed to insert_samples_VAR
 #define insert_samples(...)insert_samples_VAR((insert_samples_args){__VA_ARGS__})
+
 long get_sample(Raw_wave * wave, int nSample);
 void set_sample(Raw_wave * wave, int nSample, long value);
 void extract_samples_llist(Linked_list * result, Raw_wave * wave, int num);
